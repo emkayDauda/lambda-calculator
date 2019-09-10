ebce2ea91316663c3499bec8530de68fb95fae12
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-10 11:36:23 +0100

    :sparkles: Scaffold Numbers.js
    Export Numbers as component, import NumberButton component and set buttons up with correct number and an empty onclick handler

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 //import any components needed
+import NumberButton from './NumberButton'
 import numbers from '../../../data'
 //Import your array data to from the provided data file
 
@@ -12,6 +13,11 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {
+         stateNumbers.map(number => <NumberButton number={number} onTap={ () => {}}/>)
+       }
     </div>
   );
 };
+
+export default Numbers;
