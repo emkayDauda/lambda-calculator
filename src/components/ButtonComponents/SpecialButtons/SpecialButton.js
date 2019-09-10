8fc02b911330d0f9cd1239a08742180aeada9f1c
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-10 11:50:08 +0100

    :sparkles: Set up Specials.js and SpecialButton.js as earlier

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = ({specaialCharacter, onTap}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button onClick={onTap}>{specaialCharacter}</button>
     </>
   );
 };
+
+export default SpecialButton;
