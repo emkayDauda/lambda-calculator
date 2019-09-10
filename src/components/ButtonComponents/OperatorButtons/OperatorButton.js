cea76be268d0eba50d2e8e48d97188a75723b6b7
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-10 11:43:17 +0100

    :sparkles: Set up Operators and OperatorButton
    import and export as necessary, and add empty handler for OperatorButton

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = ({operator, onTap}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button onClick={onTap}>{operator}</button>
     </>
   );
 };
+
+
+export default OperatorButton;
