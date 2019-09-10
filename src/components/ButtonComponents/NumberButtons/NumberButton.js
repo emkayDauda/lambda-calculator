5c9843fd0f54de140974eabe7e70214a2cf998d3
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-10 11:32:58 +0100

    :alembic: Set up NumberButton
    Add params for number and function for onTap

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = ({number, onTap}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button onClick={() => onTap()}>{number}</button>
     </>
   );
 };
+
+export default NumberButton;
