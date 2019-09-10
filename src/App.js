b7275b7ae46ee7b938059c03a709e0e26c659dd5
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-10 12:06:58 +0100

    Create rudimentary handler function to update state

--- src/App.js
+++ src/App.js
@@ -17,7 +17,11 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
-  const [display, updateDisplay] = useState(0);
+  const [display, updateDisplay] = useState('0');
+
+  function handler(whatComesIn) {
+    updateDisplay(display + whatComesIn);
+  }
 
   return (
     <div className="container">
